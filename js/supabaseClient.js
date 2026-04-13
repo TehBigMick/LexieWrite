3a9c29c86a63089192fd1831cddede22c56d28a3
TehBigMick <134397911+TehBigMick@users.noreply.github.com>
2026-04-13 12:20:12 +0800

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://njuymuqeevkuqfubimio.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_uIK6LBTvH73j1fKDGRUK3w_VvetxoFB";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
